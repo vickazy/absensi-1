7274e1ccf128c92035517b8aeaba817dd06f19ee
Kiddy <kiddydhana@gmail.com>
2018-07-17 19:23:58 +0700

    Fix slash of base_url

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'http://locahost/projek_absensi';
+$config['base_url'] = 'http://locahost/projek_absensi/';
 
 $config['uri_protocol']	= 'REQUEST_URI';
 
